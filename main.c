6fae7c817ffdef771ad5f6cb1305376952ddbd6f
worthless443 <aissypewdiepie@gmail.com>
2023-05-19 01:01:54 +0530

    add default output file name: a.out

--- main.c
+++ main.c
@@ -13,19 +13,20 @@ int convert_hex_string_int(char *str) {
 int main(int argc, char **argv) {
 	int start_pos;
 	int read_only = 0;
-	char *out_fn;
-	for(int i=1;i<argc;++i)
+	char *out_fn = "a.out";
+	for(int i=1;i<argc;++i) {
 		if(0 == strcmp(argv[i],"-ro"))
 			read_only = 1;
-	
+		else if(0 == strcmp(argv[i],"-o")) 
+			out_fn = argv[++i];
+	}
 	if(!read_only) {
-		if(argc < 4 || argc > 4) {
-			fprintf(stderr, "./" STRFY(_TARGET) " <start_pos> <content> <outfile>\n");
+		if(argc < 3) {
+			fprintf(stderr, "./" STRFY(_TARGET) " <start_pos> <content> [ -o <outfile> ]\n");
 			return 1;
 		}
 	}
 	if(!read_only) {
-		out_fn = argv[3];
 		if((start_pos = convert_hex_string_int(argv[1])) == ERROR_PARSE) {
 			fprintf(stderr, STRFY(_TARGET) ":invalid hex start position:\"%s\"\n",argv[1]);
 		}
@@ -35,7 +36,6 @@ int main(int argc, char **argv) {
 			return 2;
 	}
 	if(read_only) {
-		out_fn = argv[1];
 		struct hdr_loader *f_ld = malloc(sizeof(char)*25);
 		if(read_model_fmt(out_fn, f_ld) == ERROR_FILE_NOT_FOUND)
 			return ~ERROR_FILE_NOT_FOUND;
